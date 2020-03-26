a3445b2bc6e019c2c3a7abc414f79bfb167d2c64
PavelKaraev <kraev.pashka@gmail.com>
2020-03-26 20:38:36 +0200

    create project's UI and FormHandler js module

---
+++ js/formHandler.js
@@ -0,0 +1,63 @@
+(function(window){
+  'use strict';
+  let App = window.App || {};
+  let $ = window.jQuery;
+
+  function FormHandler(selector){
+    if(!selector){
+      throw new Error('No selector provided');
+    }
+
+    this.$formElement = $(selector);
+    this.$range = this.$formElement.find('input[type="range"]');
+    this.$rangeValue = this.$formElement.find('.range-value');
+
+    this.colorRangeValue = function(rangeValue){
+      if(rangeValue < 40){
+        this.$rangeValue.css('color', 'green');
+      }else if(rangeValue < 70 && value >= 40){
+        this.$rangeValue.css('color', 'orange');
+      }else if(rangeValue >= 70){
+        this.$rangeValue.css('color', 'red');
+      }
+    }
+
+    this.colorRangeValue(this.$range.val());
+
+    if(this.$formElement.length === 0){
+      throw new Error(`Could not find element with selector: ${selector}`)
+    }
+  }
+
+  FormHandler.prototype.addSubmitHandler = function(fn){
+    console.log('Setting submit handler for form');
+    this.$formElement.on('submit', function(event){
+      event.preventDefault();
+
+      let data = {};
+      $(this).serializeArray().forEach(function(item){
+        data[item.name] = item.value;
+        console.log(`${item.name} is ${item.value}`);
+      })
+      fn(data);
+      this.reset();
+      this.elements[0].focus();
+    })
+  }
+  FormHandler.prototype.addRangeChangeHandler = function(){
+    let rangeValue = this.$rangeValue;
+    this.$range.on('input', function(){
+      let value = +$(this).val();
+      rangeValue.text(`${value}%`);
+      if(value < 40){
+        rangeValue.css('color', 'green');
+      }else if(value < 70 && value >= 40){
+        rangeValue.css('color', 'orange');
+      }else if(value >= 70){
+        rangeValue.css('color', 'red');
+      }
+    })
+  }
+  App.FormHandler = FormHandler;
+  window.App = App;
+})(window);
--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 (function(window){
   'use strict';
+  let FORM_SELECTOR = '[data-coffee-order="form"]';
   let App = window.App;
   let Truck = App.Truck;
   let DataStore = App.DataStore;
+  let FormHandler = App.FormHandler;
 
   let myTruck = new Truck('barista', new DataStore());
   window.myTruck = myTruck;
+
+  let formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  formHandler.addRangeChangeHandler();
+  console.log(formHandler);
 })(window);
